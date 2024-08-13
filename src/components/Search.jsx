f37ab4e43fdbf43eb20d8a1fd0d5c8e5c9040d24
Khaled Tanveer <khaledtanveer+gh@gmail.com>
2024-08-13 00:47:52 -0400

    new commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,66 +1,100 @@
-import "../styles/Search.scss";
 import { useState } from "react";
 import md5 from "md5";
+import "../styles/Search.scss";
+import Characters from "./Characters";
+import Comics from "./Comics";
 
 export default function Search() {
-        const [characterName, setCharacteName] = useState("");
-        const [characterData, setCharacterData] = useState(null);
-        const [comicData, setComicData] = useState(null);
+  const [characterData, setCharacterData] = useState(null);
+  const [comicData, setComicData] = useState(null);
+  const [characterName, setCharacterName] = useState("");
 
-        //const publicKey  = import.meta.env.VITE_PUBLIC_KEY;
+  const publicKey = import.meta.env.VITE_REACT_API_PUBLIC_KEY;
+  const privateKey = import.meta.env.VITE_REACT_API_PRIVATE_KEY;
+ 
 
-        const publicKey  = "94a8ad0a28c0af8860d07136b5409602";
-        const privateKey  = "c7825425d8ed3cb62e65f9752373e31c93bd86fd";
-        //const privateKey  = import.meta.env.VITE_PRIVATE_KEY;
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
+    getCharacterData();
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        getCharacterData();
-    };
+  const getCharacterData = () => {
+    setCharacterData(null);
+    setComicData(null);
 
-    const getCharacterData = () =>{
-        setCharacterData(null);
-        setComicData(null); //data will be removed with new data
+    const timeStamp = new Date().getTime();
+    const hash = generateHash(timeStamp);
 
-        const timeStamp = new Date().getTime();
-        const hash = generateHash(timeStamp);
-        const url = `https://gateway.marvel.com:443/v1/public/characters?apikey=${publicKey}&hash=${hash}&ts=${timeStamp}&nameStartsWith=${characterName}&limit=100`;
-        fetch(url)
-            .then((response) => response.json())
-            .then((result) => {
-                setCharacterData(result.data);
-                //console.log(result)
-            })
-            .catch((error) => {
-                console.log("There was an error: ", error);
-    });
+    const url = `https://gateway.marvel.com:443/v1/public/characters?apikey=${publicKey}&hash=${hash}&ts=${timeStamp}&nameStartsWith=${characterName}&limit=100`;
 
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        setCharacterData(result.data);
+        console.log(result.data);
+      })
+      .catch(() => {
+        console.log("error while getting character data");
+      });
+  };
 
-    const generateHash = (timeStamp) => {
-        return md5(timeStamp + privateKey + publicKey);
-    }
+  const getComicData = (characterId) => {
+    window.scrollTo({ top: 0, left: 0 });
 
-    const handleChange= (event) => {
-        setCharacteName(event.target.value);
-    };
+    const timeStamp = new Date().getTime();
+    const hash = generateHash(timeStamp);
 
-    const handleReset= () => {
-        //To do implement later
-    };
+    const url = `https://gateway.marvel.com:443/v1/public/characters/${characterId}/comics?apikey=${publicKey}&hash=${hash}&ts=${timeStamp}`;
 
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        setComicData(result.data);
+        console.log(result.data);
+      })
+      .catch(() => {
+        console.log("error while getting comic data");
+      });
+  };
 
-    return <>
-    <form className="search" onSubmit={handleSubmit}>
+  const handleChange = (event) => {
+    setCharacterName(event.target.value);
+  };
+
+  const generateHash = (timeStamp) => {
+    return md5(timeStamp + privateKey + publicKey);
+  };
+
+  const handleReset = () => {
+    setCharacterName("");
+    setCharacterData(null);
+    setComicData(null);
+  };
+
+  return (
+    <>
+      <form className="search" onSubmit={handleSubmit}>
         <input
-            placeholder="ENTER CHARACTER NAME"
-            type="text"
-            onChange={handleChange}
+          type="text"
+          placeholder="enter character name"
+          onChange={handleChange}
         />
         <div className="buttons">
-            <button type="submit">Get character data</button>
-            <button type="reset" className="reset" onClick={handleReset}>Reset</button>
+          <button type="submit">Get Character Data</button>
+          <button type="reset" className="reset" onClick={handleReset}>
+            Reset
+          </button>
         </div>
-    </form>
-    </>;
+      </form>
+
+      {!comicData && characterData && characterData.results[0] && (
+        <Characters data={characterData.results} onClick={getComicData} />
+      )}
+
+      {comicData && comicData.results[0] && (
+        <Comics data={comicData.results} onClick={() => {}} />
+      )}
+    </>
+  );
 }
